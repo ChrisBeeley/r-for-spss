7bede8f89a4765f7a945de9c7642ea5e76bad0fd
Beeley Chris - Senior Analyst <Chris.Beeley@nottshc.nhs.uk>
2019-05-14 15:27:51 +0100

    First commit after the first session

---
+++ README.md
@@ -0,0 +1,9 @@
+## This is a one day workshop aimed at teaching SPSS users how to use R.
+
+I run it within the NHS but I imagine it could be run in other organisations without any alterations.
+
+Do note that all the data in this repository is either made up (in the case of Dummy_SPSS.sav) or freely available online (all other datasets).
+
+Remix/ reuse for non/ commercial/ whatever.
+
+The first session was a success but I had a lot of feedback about the end about tweaking the content and to be honest I'm not even sure the material is presented in the correct order. I'm releasing it now because somebody asked and intend to make it better over the coming months (there is another session planned in June, so before then).
---
+++ exercises.R
@@ -0,0 +1,31 @@
+
+## 1. read in the csv file
+
+# copy the code in here
+
+## 2. Investigate the data frame
+
+# how many variables?
+
+# list the type of each
+
+# how many rows?
+
+# what is the 6th question in the dataset?
+
+# what is the question that comes 16th from last in the dataset?
+
+# 3. Descriptive analysis
+
+# conduct a descriptive analysis of the NHANES data, including numerical and categorical data
+
+# 4. Correlation analyis
+
+# Continue to look at correlations within the NHANES data using appropriate parametric/
+# non parametric methods (plotting the distribution where necessary)
+
+# 5. dummy dataset
+
+# using all the tools that we've looked at today conduct descriptive and inferential 
+# analyses of the dummy dataset
+
